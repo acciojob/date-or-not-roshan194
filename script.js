6c69b1b3777d6f3bad62e6156f916e13fe0caa2a
Roshan <71066704+roshan194@users.noreply.github.com>
2025-04-04 21:59:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
-() => {
-  const dateStr = "2023-08-11"; // Return a valid date string
-
-  cy.visit(baseUrl, {
-    onBeforeLoad(win) {
-      cy.stub(win, "prompt").onFirstCall().returns(dateStr); // Stub prompt with a valid date string
-    }
-  });
-
-  cy.on("window:alert", (str) => {
-    expect(str).to.equal("true"); // Expect alert to be the string "true"
-  });
+var isDate = function(input) {
+	return ! isNaN(Date.parse(input));
 }
 
 
